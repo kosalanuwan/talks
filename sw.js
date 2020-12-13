61df3b38e8e11e8ad6d11cd9d1cab60423d07ebf
kosalanuwan <kosalanuwan@users.noreply.github.com>
2020-12-13 07:47:07 +0000

    deploy: kosalanuwan/gh-site-talks@2e33ee5500f8b3942e4565af1baa506035d470e2

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/talks/assets/css/main.css?202012110639",
+        "/talks/assets/css/main.css?202012130747",
         "/talks/"
       ]);
     })
